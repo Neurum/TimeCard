b5697cc2abba713cdb9fd173381ef0eb374f7c10
Curt House <fullstackwebdev78@gmail.com>
2023-02-22 16:59:50 -0500

    Initial commit with login screen complete

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
+html {
+  scroll-behavior: smooth;
+  scroll-padding: 5.5rem;
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,15 @@
+<template>
+  <main class="bg-gray-300 w-screen h-screen">
+    <LoginModal />
+  </main>
+</template>
+
+<script>
+import LoginModal from '../components/LoginModal.vue';
+
+export default {
+  components: {
+    LoginModal,
+  },
+};
+</script>
